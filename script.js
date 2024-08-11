d4f5ef3756c14460b244b21606654edc9f9e4983
Nidhi Chahal <135501853+flexnidhi@users.noreply.github.com>
2024-08-11 14:33:33 +0530

    Add files via upload
    Add initial file

---
+++ script.js
@@ -0,0 +1,20 @@
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.text-img', { 
+    origin: 'left',
+    opacity: 0,
+    distance: '50px'
+});
+ScrollReveal().reveal('.freedom', { 
+    origin: 'right',
+    opacity: 0,
+    distance: '50px'
+});
+
+
